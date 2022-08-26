2edf37013415c14b2993ba6bbe289c95044e5ca2
Pedro Noda <100450891+PedroNoda@users.noreply.github.com>
2022-08-25 22:49:47 -0300

    Update Ex3.java

--- PROVA/Ex3.java
+++ PROVA/Ex3.java
@@ -1,46 +1,37 @@
-package Prova;
-
 import java.util.Scanner;
 
 public class Ex3 {
 	public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	double massa;
-	int tempo=0;
-	int hora=0, minuto=0, segundo=0;
-		
-	System.out.println("Informe a massa inicial do \ncomponente radioativo (gramas): ");
+		Scanner sc = new Scanner(System.in);
+		double massa;
+		int tempo = 0;
+		int hora = 0, minuto = 0, segundo = 0;
+
+		System.out.println("Informe a massa inicial do \ncomponente radioativo (gramas): ");
 		massa = sc.nextDouble();
-		 
+
 		while (massa >= 0.5) {
-			System.out.println("massa " +massa);
-			massa = massa/2;
-			System.out.println("massa " +massa);
-			System.out.println("--------------------------------");
-			System.out.println("tempo " +tempo);
+			massa = massa / 2;
 			tempo = tempo + 50;
-			System.out.println("tempo " +tempo);
-			
-			System.out.println("--------------------------------");
+		}
+
+		while (tempo >= 3600) {
+			hora++;
+			tempo = tempo - 3600;
+		}
+
+		while (tempo >= 60) {
+			minuto++;
+			tempo = tempo - 60;
+		}
+
+		while (tempo < 60 && tempo >0) {
+			segundo++;
+			tempo = tempo - 1;
 		}
 		
-			while (tempo >= 3600){
-				hora++;
-				tempo = tempo- 3600;
-			}
+		System.out.println("Horas: " + hora + " | Minutos: " + minuto + " | Segundos: " + segundo);
 
-			while (tempo >= 60){
-				minuto++;
-				tempo = tempo - 60;
-			}
-	
-			while (tempo < 60) {
-				segundo++;
-				tempo= tempo-tempo;
-			}
-		
-		System.out.println("Horas: " + hora + " | Minutos: " + minuto +" | Segundos: " + segundo);
-		
-	sc.close();
+		sc.close();
 	}
 }
